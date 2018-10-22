3374bafe1f1fe09dc818b68c6ccaf86812e542fe
chengxiaobo <bftv123456>
2018-10-22 13:17:25 +0800

    项目搭建

--- app/src/main/java/com/example/test/testhencoder/MainActivity.kt
+++ app/src/main/java/com/example/test/testhencoder/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.test.testhencoder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test.testone.TestOneDrawActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * hencoder主Activity
+ * @author chengxiaobo
+ * @time 2018/10/22 13:15
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv01TestOne.setOnClickListener {
+            startActivity(Intent(this@MainActivity, TestOneDrawActivity::class.java))
+        }
     }
 }
---
+++ lib.view.testone/src/main/java/com/example/test/testone/TestOneDrawActivity.kt
@@ -0,0 +1,17 @@
+package com.example.test.testone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * hencoder07 绘制基础以及图像和文字的测量
+ * @author chengxiaobo
+ * @time 2018/10/22 13:15
+ */
+class TestOneDrawActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_one_draw)
+    }
+}
