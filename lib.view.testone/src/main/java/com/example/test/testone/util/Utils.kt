ba1e13931888a31ec5cc692d786a94ebcef704de
chengxiaobo <bftv123456>
2018-10-23 19:14:01 +0800

    圆形图像未完成

--- lib.view.testone/src/main/java/com/example/test/testone/util/Utils.kt
+++ lib.view.testone/src/main/java/com/example/test/testone/util/Utils.kt
@@ -1,6 +1,9 @@
 package com.example.test.testone.util
 
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.view.View
 
 /**
  * 工具类
@@ -14,4 +17,14 @@ fun dp2px(dp: Float): Float {
 
 fun getAngleRadians(angle: Float): Double {
     return angle * Math.PI / 180
+}
+
+fun View.getBitmap(widthPX: Int, resourceId: Int): Bitmap {
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+    BitmapFactory.decodeResource(this.resources, resourceId, options)
+    options.inJustDecodeBounds = false
+    options.inDensity = options.outWidth
+    options.inTargetDensity = widthPX
+    return BitmapFactory.decodeResource(this.resources, resourceId, options)
 }
---
+++ lib.view.testone/src/main/java/com/example/test/testone/view/CircleImageView.kt
@@ -0,0 +1,63 @@
+package com.example.test.testone.view
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.example.test.testone.R
+import com.example.test.testone.util.dp2px
+import com.example.test.testone.util.getBitmap
+
+/**
+ * hencoder07 圆形图片
+ * @author chengxiaobo
+ * @time 2018/10/22 16:15
+ */
+class CircleImageView : View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var centerX = 0.0f
+    private var centerY = 0.0f
+    private var imageWidth = 0.0f
+
+    private var padding: Int = dp2px(5.0f).toInt()
+
+    private lateinit var paint: Paint
+    private var bitmap: Bitmap
+    private lateinit var imageViewRect: RectF
+    private lateinit var bitmapRect: Rect
+    private var xFermode: Xfermode
+
+    init {
+        bitmap = getBitmap(imageWidth.toInt(), R.drawable.cheng)
+        xFermode = Xfermode()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        paint = Paint()
+        centerX = width / 2.0f
+        centerY = height / 2.0f
+
+        imageWidth = width / 3.0f
+
+        bitmapRect = Rect(0, 0, bitmap.width, bitmap.height)
+        imageViewRect = RectF(centerX - imageWidth, centerY - imageWidth, centerX + imageWidth, centerY + imageWidth)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.xfermode = xFermode
+        val saved = canvas?.saveLayer(imageViewRect, paint)
+        canvas?.drawBitmap(bitmap, bitmapRect, imageViewRect, paint)
+        canvas?.drawCircle(centerX, centerY, imageWidth, paint)
+        paint.xfermode = null
+        saved?.let {
+            canvas.restoreToCount(saved)
+        }
+    }
+}
