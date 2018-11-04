1922e671e90715794729d0bb2ea9a30af88861d4
chengxiaobo <chengxiaobo0416@sina.com>
2018-11-04 16:00:17 +0800

    canvasCamera1

---
+++ lib.view.testone/src/main/java/com/example/test/testone/view/L07/TestCanvasCamera.kt
@@ -0,0 +1,64 @@
+package com.example.test.testone.view.L07
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.example.test.testone.R
+import com.example.test.testone.util.dp2px
+import com.example.test.testone.util.getBitmap
+
+/**
+ * 学习关于Canvas的Camera
+ *
+ * @author chengxiaobo
+ * @time 2018/11/4 13:42
+ */
+class TestCanvasCamera : View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var imageWidth = 0.0f
+    private var centerX = 0.0f
+    private var centerY = 0.0f
+
+    private lateinit var sourceRect: Rect
+    private lateinit var destRect: Rect
+    private lateinit var bitmap: Bitmap
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val camera: Camera = Camera()
+
+    init {
+        camera.setLocation(0.0f, 0.0f, -dp2px(12.0f))
+        camera.rotateX(45.0f)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        imageWidth = width / 2.5f
+        centerX = width / 2.0f
+        centerY = height / 2.0f
+
+        bitmap = getBitmap(imageWidth.toInt(), R.drawable.cheng)
+        destRect = Rect((centerX - imageWidth).toInt(), (centerY - imageWidth).toInt(), (centerX + imageWidth).toInt(), (centerY + imageWidth).toInt())
+        val bitmapWidth = if (bitmap.width > bitmap.height) bitmap.height else bitmap.width
+        sourceRect = Rect(0, 0, bitmapWidth, bitmapWidth)
+
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.save()
+        canvas?.translate(centerX, centerY)
+        camera.applyToCanvas(canvas)
+        canvas?.translate(-centerX, -centerY)
+        canvas?.drawBitmap(bitmap, sourceRect, destRect, paint)
+        canvas?.restore()
+
+    }
+}
